45d4d6cbcf5394db7fb48063d3ab22e55cc8922f
kanishka0325 <purna@web3genes.com>
2023-05-29 14:17:17 +0530

    bugfixed

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-#give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/my-project
+echo 'run application_start.sh: ' >> /home/ec2-user/api-sever/deploy.log
 
-#navigate into our working directory where we have all our github files
-cd /home/ec2-user/my-project
-
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-
-#install node modules
-npm install
-
-#start our node app in the background
-node app.js > app.out.log 2> app.err.log < /dev/null & 
+echo 'pm2 restart test-api' >> /home/ec2-user/api-sever/deploy.log
+sudo pm2 restart test-api >> /home/ec2-user/api-sever/deploy.log
+sudo pm2 save >> /home/ec2-user/api-sever/deploy.log
